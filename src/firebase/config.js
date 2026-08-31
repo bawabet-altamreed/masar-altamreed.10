d61d27fe0bf469a3fe3fe11d70118f5283960d2b
bawabet-altamreed <medo.beso2023@gmail.com>
2026-08-31 12:27:01 +0300

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+// Masar Al-Tamreed
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyALNUiUxnUDW5MDBxXOs8VUjDSCrFgkYkk",
+    authDomain: "masar-altamreed-62334.firebaseapp.com",
+    projectId: "masar-altamreed-62334",
+    storageBucket: "masar-altamreed-62334.firebasestorage.app",
+    messagingSenderId: "313911491183",
+    appId: "1:313911491183:web:a8492030f8f18568e2eb9a",
+    measurementId: "G-HL35BEEKK2"
+};
