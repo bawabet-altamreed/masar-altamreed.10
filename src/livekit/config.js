ba3a4f8fa013ce8b1c73a07efb0a8bf06a421d24
bawabet-altamreed <medo.beso2023@gmail.com>
2026-09-02 17:10:46 +0300

    Create config.js

---
+++ src/livekit/config.js
@@ -0,0 +1,11 @@
+export const LIVEKIT_CONFIG = {
+
+    // LiveKit Cloud WebSocket URL
+    serverUrl:
+        "wss://masar-al-tamreed-yao3ibc5.livekit.cloud",
+
+    // Development Token Server
+    tokenServerId:
+        "masaraltamreed-5uoy7c"
+
+};
