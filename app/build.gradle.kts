ea4799bf8ce225d74b9ff3d4dd0b687e94a66df7
bawabet-altamreed <medo.beso2023@gmail.com>
2026-09-04 12:23:07 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
